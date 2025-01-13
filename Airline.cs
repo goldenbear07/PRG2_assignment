e271ee54ccb23ba0e0f8c2cc9f13adc751239e19
Keng Siong Toh <s10267107@connect.np.edu.sg>
2025-01-14 01:24:58 +0800

    created Airline.cs

---
+++ Airline.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public Dictionary<string, Flight> Flights = new Dictionary<string, Flight>();
+
+        public Airline() { }
+
+        public Airline(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public bool AddFlight(Flight flight)
+        {
+            if (Flights.ContainsKey(flight.FlightNumber))
+            {
+                return false;
+            }
+            Flights[flight.FlightNumber] = flight;
+            return true;
+        }
+
+        public double CalculateFees()
+        {
+            double totalFees = 0;
+            foreach(Flights flights in Flights.Values)
+            {
+                totalFees += flights.CalculateFees();
+            }
+            return totalFees;
+        }
+
+        public bool RemoveFlight(Flight flight)
+        {
+            if (Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights.Remove(flight.FlightNumber);
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Airline Name: {Name}\nAirline Code: {Code}";
+        }
+    }
+}

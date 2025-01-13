21f0a17696987515e7f56f0c0d9cb3e22f2eeeeb
Keng Siong Toh <s10267107@connect.np.edu.sg>
2025-01-14 01:31:56 +0800

    created Flight.cs

---
+++ Flight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    abstract class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
+
+        public abstract double CalculateFees();
+
+        public override string ToString()
+        {
+            return $"{FlightNumber} from {Origin} to {Destination} at {ExpectedTime} is {Status}";
+        }
+    }
+}

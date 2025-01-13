3e993e952f4d312c7fc05162cab5236aadfb3003
Keng Siong Toh <s10267107@connect.np.edu.sg>
2025-01-14 01:09:10 +0800

    created Terminal.cs

---
+++ Terminal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines = new Dictionary<string, Airline>();
+
+        public Dictionary<string, Flight> Flights = new Dictionary<string, Flight>();
+
+        public Dictionary<string, BoardingGate> BoardingGates = new Dictionary<string, BoardingGate>();
+
+        public Dictionary<string, double> GateFees = new Dictionary<string, double>();
+
+        public Terminal() { }
+
+        public Terminal(string terminalName)
+        {
+            TerminalName = terminalName;
+        }
+
+        public bool AddAirline(Airline airline)
+        {
+            if (Airlines.ContainsKey(airline.Code))
+            {
+                return false;
+            }
+            Airlines[airline.Code] = airline;
+            return true;
+        }
+
+        public bool AddBoardingGate(BoardingGate gate)
+        {
+            if (BoardingGates.ContainsKey(gate.gateName))
+            {
+                return false;
+            }
+            BoardingGates[gate.gateName] = gate;
+            return true;
+        }
+
+        public Airline GetAirlineFromFlight(Flight flight)
+        {
+            foreach (Airline airline in Airlines.Values)
+            {
+                if (airline.Flights.ContainsKey(flight.FlightNumber))
+                {
+                    return airline;
+                }
+            }
+            return null;
+        }
+
+        public void PrintAirlineFees() //method to calculate the amount of money the airline has to pay at the boardingGate based on the services / arrival and departing fees...
+        {
+            foreach(Airline airlines in Airlines.Values)
+            {
+                Console.WriteLine($"{airlines} fee: {airlines.CalculateFees()}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Terminal name: {TerminalName}";
+        }
+
+    }
+}
